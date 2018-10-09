98136b45a5befe754b83652fde6e463c4b3c1e5a
Alexandr Zanoga <zanoga1@gmail.com>
2018-10-09 17:46:31 +0300

    3-module 1-task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,7 +4,25 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+
+'use strict';
+
+let data = [{
+    "balance": "$1,825.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "name": "Golden Branch",
+    "gender": "male",
+    "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+}];
+
 function showSalary(data, age) {
+    data.forEach(item => {
+        if(item.age <= age) {
+            console.log(item.name + ', ' + item.balance);
+        }
+    });
 }
 
-
+showSalary(data, 21);
