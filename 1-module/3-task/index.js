2dd1a1d08086732ee5951d92ce46a43f68ff451c
Alexandr Zanoga <zanoga1@gmail.com>
2018-11-04 21:24:29 +0200

    1-module 3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,17 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
 
+let inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
+let inputData2 = '25,-1,-234,4,   1000';
+function getMinMax(str) {
+    let srting = str.match(/-?\d+(\.\d+)?/gi);
+    parseFloat(srting.sort(function(a, b) {
+        return a - b;
+    }));
+
+    return srting[0] + ', ' + srting[srting.length - 1];
 }
 
+console.log(getMinMax(inputData) === '-5.8, 73');
+console.log(getMinMax(inputData2) === '-234, 1000');
