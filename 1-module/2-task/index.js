4eccef4c2b101410f09cb861e04a083b324e5292
Alexandr Zanoga <zanoga1@gmail.com>
2018-11-11 20:46:14 +0200

    1-module 2-task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -3,15 +3,21 @@
 /**
  * Эту функцию трогать не нужно
  */
-function print (text) {
+ function print (text) {
     alert(text);
 }
 
 /**
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
+ name != null && name.length >=4 && name.indexOf(' ') < 0
  */
-function isValid (name) {
+function isValid(name) {
+    if(typeof name !== undefined && name != null) {
+        if(/\s/g.test(name) === false && name.length > 3) {
+            return true;
+        } else return false;
+    }
 }
 
 function sayHello () {
@@ -25,4 +31,3 @@ function sayHello () {
 }
 
 sayHello();
-
