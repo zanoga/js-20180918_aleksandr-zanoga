6c2eade066095738493ba5b3dbb7d027346debf8
Alexandr Zanoga <zanoga1@gmail.com>
2018-11-18 23:42:46 +0200

    1-module 3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -9,13 +9,14 @@
 let inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
 let inputData2 = '25,-1,-234,4,   1000';
 function getMinMax(str) {
+    let o = {};
     let srting = str.match(/-?\d+(\.\d+)?/gi);
     parseFloat(srting.sort(function(a, b) {
         return a - b;
     }));
-
-    return srting[0] + ', ' + srting[srting.length - 1];
+    o.min = parseFloat(srting[0]);
+    o.max = parseFloat(srting[srting.length - 1]);
+    return o;
 }
 
-console.log(getMinMax(inputData) === '-5.8, 73');
-console.log(getMinMax(inputData2) === '-234, 1000');
+console.log(getMinMax(inputData));
